cbaff9f7354ea9d84a33183dd540b5d099013380
harold <2440800380@qq.com>
2025-01-10 05:58:00 +0800

    refact: move re exports deps to prelude

--- src/lib.rs
+++ src/lib.rs
@@ -7,31 +7,8 @@ pub mod logger;
 pub mod middle;
 #[cfg(feature = "err")]
 pub mod err;
+pub mod prelude;
 
 pub use server::*;
 pub use route::*;
 pub use logger::*;
-
-
-pub use log;
-pub use env_logger;
-pub use axum;
-pub use tokio;
-pub use thiserror;
-pub use anyhow;
-pub use serde;
-pub use serde_json;
-
-#[cfg(feature = "sqlx")]
-pub use sqlx;
-#[cfg(feature = "sqlx_macro_ex")]
-pub use sqlx_macro_ex;
-
-#[cfg(feature = "clap")]
-pub use clap;
-#[cfg(feature = "sha256")]
-pub use sha256;
-#[cfg(feature = "regex")]
-pub use regex;
-#[cfg(feature = "once_cell")]
-pub use once_cell;
---
+++ src/prelude.rs
@@ -0,0 +1,22 @@
+pub use log;
+pub use env_logger;
+pub use axum;
+pub use tokio;
+pub use thiserror;
+pub use anyhow;
+pub use serde;
+pub use serde_json;
+
+#[cfg(feature = "sqlx")]
+pub use sqlx;
+#[cfg(feature = "sqlx_macro_ex")]
+pub use sqlx_macro_ex;
+
+#[cfg(feature = "clap")]
+pub use clap;
+#[cfg(feature = "sha256")]
+pub use sha256;
+#[cfg(feature = "regex")]
+pub use regex;
+#[cfg(feature = "once_cell")]
+pub use once_cell;
